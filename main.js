9449a291d635ab9ae9a620159a770e1fe6fedc6a
liukuai <937613925@qq.com>
2024-03-08 15:05:22 +0800

    更换模型；添加光照

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-
+import { MapControls } from 'three/addons/controls/MapControls.js'
 
 // 场景
 const scene = new THREE.Scene()
 
+// 灯光
 scene.add(new THREE.AmbientLight(0xFFFFFF))
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 10 )
+directionalLight.position.set(10, 10, 10).normalize()
+scene.add( directionalLight )
 
 // 相机
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-camera.position.set(0, 0, 1)
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000)
+camera.position.set(10, 10, 20)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+scene.add(camera)
 
 // 渲染器
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({antialias: true})
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
@@ -24,15 +30,33 @@ document.body.appendChild( renderer.domElement )
 
 // 导入模型
 const loader = new GLTFLoader()
-loader.load('./3d/chicken_gun_deserttown2/scene.gltf', function ( gltf ) {
-  scene.add(gltf.scene)
+loader.load('./3d/chinese_old_hotel/scene.gltf', function ( gltf ) {
+  const model = gltf.scene
+  model.position.set(0, 0, 0)
+  scene.add(model)
 }, undefined, function ( error ) { 
   console.error( error )
 })
 
+// 控制
+const controls = new MapControls( camera, renderer.domElement )
+controls.enableDamping = true
+controls.dampingFactor = 0.30
+controls.screenSpacePanning = false
+controls.autoRotate = false
+controls.enableZoom = true
+controls.zoomSpeed = 2.0
+controls.minZoom = Infinity
+controls.maxZoom = Infinity
+controls.enablePan = true
+controls.panSpeed = 5.0
+
 // 循环渲染场景
 function animate() {
   requestAnimationFrame( animate )
+
+  controls.update()
+
   renderer.render( scene, camera )
 }
 animate()
