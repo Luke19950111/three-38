794d5a889430bd2f68f5db78f652d8e6d186cc4d
liukuai <937613925@qq.com>
2024-03-08 11:14:48 +0800

    创建物品

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ const scene = new THREE.Scene()
 
 // 相机
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+camera.position.set(0, 0, 5)
 
 // 渲染器
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
+
+// 创建物品
+const geometry = new THREE.BoxGeometry( 1, 1, 1 )
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+const cube = new THREE.Mesh( geometry, material )
+scene.add( cube )
